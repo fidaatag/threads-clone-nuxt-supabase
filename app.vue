85d715d322810ad71381b1010c8e24bf00bddf6b
fidaatag <fidaa2fidaa@gmail.com>
2024-07-09 16:26:05 +0700

    create-post-overlay

--- app.vue
+++ app.vue
@@ -2,5 +2,17 @@
   <div class="bg-black fixed w-[calc(100%+1px)] h-full">
     <!-- <NuxtRouteAnnouncer /> -->
     <NuxtPage />
+
+    <CreatePost
+      :class="[
+        {'max-h-[100vh] transition-all duration-200 ease-in visible': userStore.isMenuOverlay },
+        {'max-h-0 transition-all duration-200 ease-out invisible': !userStore.isMenuOverlay },
+      ]"
+    />
   </div>
 </template>
+
+<script setup>
+  import { useUserStore } from "~/stores/user";
+  const userStore = useUserStore()
+</script>
